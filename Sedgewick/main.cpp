6d778655209030cdbeb3ad0d358c0ce519a56c1a
Max Boytsov <max@MacBook-Air-Max.local>
2014-01-30 03:10:35 +0400

    First commit

---
+++ Sedgewick/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sedgewick
+//
+//  Created by Max Boytsov on 30/01/14.
+//  Copyright (c) 2014 Max Boytsov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
