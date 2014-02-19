acf843438026da31f1a1fbe02ccdc8427d35699e
Max Boytsov <max@MacBook-Air-Max.local>
2014-02-20 03:23:15 +0400

    updated

--- main.cpp
+++ main.cpp
@@ -12,14 +12,106 @@ using Sedgewick::Item;
 using Sedgewick::Node;
 using Sedgewick::Link;
 
+class String {
+private:
+    char* s;
+public:
+    String(char*);
+    String(const String&);
+    virtual ~String();
+    String& operator=(const String&);
+    String& operator+(const String&) const;
+    void Dump(std::ostream& os);
+};
+
+String::String(char *str) : s(NULL)
+{
+    if (str == NULL) {
+        s = new char[1];
+        *s = '\0';
+    } else {
+        s = new char[strlen(str) + 1];
+        strcpy(s, str);
+    }
+}
+
+String::String(const String& str) : s(NULL)
+{
+    s = new char[strlen(str.s) + 1];
+    strcpy(s, str.s);
+}
+
+String::~String()
+{
+    delete s;
+}
+
+String& String::operator=(const String& str)
+{
+    if (&str == this) return *this;
+    delete s;
+    s = new char[strlen(str.s) + 1];
+    strcpy(s, str.s);
+    return *this;
+}
+
+String& String::operator+(const String& s1) const
+{
+    //char* s2 = new char[strlen(s1.s) + strlen(s) + 1];
+    //strcat(s2, s1, s);
+}
+
+void String::Dump(std::ostream& os)
+{
+    os << "@:\"" << s << "\"";
+}
+
+class MyObject {
+public:
+    ~MyObject()
+    {
+        std::cout << "!!!! delete" << std::endl;
+    }
+};
+
 int main()
 {
+    String* s1 = new String("Hello");
+    String* s2 = new String("Goodby");
+    *s2 = *s1;
+    delete s1;
+    s2->Dump(cout);
+    delete s2;
+
+    return 0;
+
+    std::shared_ptr<MyObject> p1(new MyObject);
+    std::shared_ptr<MyObject> p2;
+
+    p2 = p1;
+
+    if (p2)
+        std::cout << "!1" << std::endl;
+
+
+
+    p2 = NULL;
+    p1 = NULL;
+
+
+
+    std::cout << "end" << std::endl;
+
+
+    return 0;
+
+
     Item x = { "Item name" };
     Link t = new Node(x, t);
     cout << x.name << endl;
     Link next = t->next;
-    Item i = next->item;
-    cout << "!!!" << i.name << endl;
+    //Item i = next->item;
+    //cout << "!!!" << i.name << endl;
 
     Point a = { 0, 0 };
     Point b = { 3, 4 };
